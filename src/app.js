27160bc343480862b611d3c1a4160ba761f5e174
Arseni Zemskow <scvsh@vetki.us>
2017-12-16 19:47:31 +0300

    Refactor, update modules

--- src/app.js
+++ src/app.js
@@ -1,30 +1,35 @@
-let ko = require('knockout');
+import './style.less';
+import $ from 'jquery';
+import ko from 'knockout';
+import './map.js';
 
-class PeopleViewModel {
-    constructor() {
-        //Inital data
-        this.people = ko.observableArray([
-            { name: 'John Smith', amount: 12.01},
-            { name: 'Jeff Smithe', amount: 0.5},
-            { name: 'Jarrod Smythe', amount: 1.0},
-        ]);
 
-        //Bound remove to the constructor as $parent changes the context of this in ES6 classes.
-        this.removePerson = value => {
-            this.people.remove(value);
-        };
-    }
 
-    addPerson() {
-        //Get Value from text inputs.
-        let name = document.getElementsByName('name')[0].value;
-        let amount = document.getElementsByName('amount')[0].value;
+//class PeopleViewModel {
+//constructor() {
+////Inital data
+//this.people = ko.observableArray([
+//{ name: 'John Smith', amount: 12.01},
+//{ name: 'Jeff Smithe', amount: 0.5},
+//{ name: 'Jarrod Smythe', amount: 1.0},
+//]);
 
-        if (name !== undefined && amount !== undefined) {
-            //Add to ko array
-            this.people.push({name: name, amount: amount});
-        }
-    }
-}
+////Bound remove to the constructor as $parent changes the context of this in ES6 classes.
+//this.removePerson = value => {
+//this.people.remove(value);
+//};
+//}
 
-ko.applyBindings(new PeopleViewModel());
+//addPerson() {
+////Get Value from text inputs.
+//let name = document.getElementsByName('name')[0].value;
+//let amount = document.getElementsByName('amount')[0].value;
+
+//if (name !== undefined && amount !== undefined) {
+////Add to ko array
+//this.people.push({name: name, amount: amount});
+//}
+//}
+//}
+
+//ko.applyBindings(new PeopleViewModel());
--- webpack.config.js
+++ webpack.config.js
@@ -3,56 +3,82 @@ const path = require('path');
 const webpack = require('webpack');
 const pkg = require(path.join(process.cwd(), 'package.json'));
 const shouldMininimize = process.argv.indexOf('--min') !== -1;
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractLess = new ExtractTextPlugin({
+    filename: 'style.css',
+    disable: process.env.NODE_ENV === 'development',
+});
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const standardConfig = {
-  devtool: 'source-map',
-  entry: './src/app.js',
+    devtool: 'source-map',
+    entry: './src/app.js',
     output: {
-      path: './dist',
-      filename: 'app.js'
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js',
     },
-  module: {
-    noParse: /node_modules\/knockout\/build\/output\/*.js/,
-    loaders: [{
-      test: /\.scss$/,
-      loaders: ['style', 'css', 'sass']
-    }, {
-      test: /\.html$/,
-      loader: 'html'
-    }, {
-      test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-      loader: 'file'
-    }, {
-      test: /\.js$/,
-      loader: 'babel',
-      query: {
-        presets: ['babel-preset-es2015']
-      }
-    }]
-  },
-  devServer: {
-    port: 8000,
-    contentBase: 'src/',
-    inline: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      hash:true,
-      inject: false
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true
-    })
-  ]
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: extractLess.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                        },
+                        {
+                            loader: 'less-loader',
+                        },
+                    ],
+                    // use style-loader in development
+                    fallback: 'style-loader',
+                }),
+            },
+        ],
+        noParse: /node_modules\/knockout\/build\/output\/*.js/,
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['babel-preset-es2015'],
+                },
+            },
+        ],
+    },
+    devServer: {
+        port: 3000,
+        contentBase: 'src/',
+        inline: false,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            hash: true,
+            inject: false,
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            include: /\.min\.js$/,
+            minimize: true,
+        }),
+        extractLess,
+    ],
 };
 
 if (shouldMininimize) {
-  Object.assign(standardConfig.entry, {
-    'dist/index.min.js': './src/index.js'
-  });
+    Object.assign(standardConfig.entry, {
+        'dist/app.min.js': './src/app.js',
+    });
 }
 
 module.exports = standardConfig;
